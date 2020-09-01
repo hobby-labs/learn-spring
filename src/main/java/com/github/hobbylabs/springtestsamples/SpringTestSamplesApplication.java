799ccbbd9e77e1dbcf01bb14192e986d60c3dc74
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-01 23:22:11 +0900

    Added spring resources

---
+++ src/main/java/com/github/hobbylabs/springtestsamples/SpringTestSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.springtestsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hobbylabs/springtestsamples/SpringTestSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.springtestsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
