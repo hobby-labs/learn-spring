4388d0d6721b02f95397b826a9022dd7eac0a40f
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-06 09:45:31 +0900

    Renew project

--- src/main/java/com/github/hobbylabs/springtestsamples/SpringtestsamplesApplication.java
+++ src/main/java/com/github/hobbylabs/springtestsamples/SpringtestsamplesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTestSamplesApplication {
+public class SpringtestsamplesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTestSamplesApplication.class, args);
+		SpringApplication.run(SpringtestsamplesApplication.class, args);
 	}
 
 }
--- src/main/java/com/github/hobbylabs/springtestsamples/entity/ResponseData.java
+++
@@ -1,9 +0,0 @@
-package com.github.hobbylabs.springtestsamples.entity;
-
-import lombok.Data;
-
-@Data
-public class ResponseData {
-    private int id;
-    private String name;
-}
