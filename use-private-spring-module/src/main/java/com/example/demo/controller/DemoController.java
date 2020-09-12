e7aaf1da1c02317c44860a53c80fb6ff9519f50a
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-13 00:54:26 +0900

    Updated

---
+++ use-private-spring-module/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.github.hobbylabs.spring.learnspringmodule.service.LearnSpringModuleService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "com.github.hobbylabs.spring.learnspringmodule")
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @Data
+    private static class DemoResponse {
+        private int id;
+        private String message1;
+        private String message2;
+    }
+
+    @Autowired
+    private LearnSpringModuleService learnSpringModuleService;
+
+    @GetMapping
+    public DemoResponse hello() {
+        DemoResponse response = new DemoResponse();
+        response.setId(1);
+        response.setMessage1(learnSpringModuleService.getHelloWorld());
+        response.setMessage2(learnSpringModuleService.message());
+
+        return response;
+    }
+}
---
+++ use-private-spring-module/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import com.example.demo.controller.DemoController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Autowired
+	private DemoController controller;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
