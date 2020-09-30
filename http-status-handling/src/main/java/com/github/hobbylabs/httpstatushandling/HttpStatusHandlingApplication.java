a6b4263d0b9e9d1cc1455219be3711b08c82fa06
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-30 22:43:56 +0900

    Added new project

---
+++ http-status-handling/src/main/java/com/github/hobbylabs/httpstatushandling/HttpStatusHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.httpstatushandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpStatusHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpStatusHandlingApplication.class, args);
+	}
+
+}
