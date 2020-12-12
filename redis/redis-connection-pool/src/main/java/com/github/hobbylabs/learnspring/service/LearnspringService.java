14e1113d19c4e9c4805e78f79425b74c95d70741
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-12-12 12:17:22 +0000

    Clone redis project

---
+++ redis/redis-connection-pool/src/main/java/com/github/hobbylabs/learnspring/service/LearnspringService.java
@@ -0,0 +1,33 @@
+package com.github.hobbylabs.learnspring.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LearnspringService {
+
+    private static Logger logger = LoggerFactory.getLogger(LearnspringService.class);
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private static final String KEY_COUNTER = "counter";
+
+    public int incrementCounter() {
+        String resultString = redisTemplate.opsForValue().get(KEY_COUNTER);
+
+        if (resultString == null) {
+            resultString = "0";
+        }
+        int result = Integer.parseInt(resultString);
+        ++result;
+
+        redisTemplate.opsForValue().set(KEY_COUNTER, String.valueOf(result));
+        logger.info("Redis counter: {}", String.valueOf(result));
+
+        return result;
+    }
+}
