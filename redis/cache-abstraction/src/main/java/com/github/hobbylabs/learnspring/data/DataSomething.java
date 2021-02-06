3f69b11350d01b2645a2a26582921b11f274afba
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-02-06 21:10:42 +0900

    Added cache-abstraction

---
+++ redis/cache-abstraction/src/main/java/com/github/hobbylabs/learnspring/data/DataSomething.java
@@ -0,0 +1,9 @@
+package com.github.hobbylabs.learnspring.data;
+
+import lombok.Data;
+
+@Data
+public class DataSomething {
+    private long id;
+    private String description;
+}
---
+++ redis/cache-abstraction/src/test/java/com/github/hobbylabs/learnspring/LearnspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
