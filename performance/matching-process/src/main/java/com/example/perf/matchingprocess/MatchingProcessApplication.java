6f799895d194b1df1180e2abe6c3bd1c56a4b7f8
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-04-18 12:12:11 +0900

    Added matching-process

---
+++ performance/matching-process/src/main/java/com/example/perf/matchingprocess/MatchingProcessApplication.java
@@ -0,0 +1,13 @@
+package com.example.perf.matchingprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingProcessApplication.class, args);
+	}
+
+}
---
+++ performance/matching-process/src/test/java/com/example/perf/matchingprocess/MatchingProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perf.matchingprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
