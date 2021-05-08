f7600520c8028e7f49817cda67bd7136025cbcb9
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-08 21:23:13 +0900

    Added new project

---
+++ redis/lettuce-and-thread-num/src/main/java/com/github/hobbylabs/lettuceandthreadnum/LettuceAndThreadNumApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.lettuceandthreadnum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettuceAndThreadNumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettuceAndThreadNumApplication.class, args);
+	}
+
+}
---
+++ redis/lettuce-and-thread-num/src/test/java/com/github/hobbylabs/lettuceandthreadnum/LettuceAndThreadNumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.lettuceandthreadnum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettuceAndThreadNumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
