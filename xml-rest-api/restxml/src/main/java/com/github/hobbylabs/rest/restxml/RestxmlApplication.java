eaad961dc25520dda722df8d90ddf84971638f52
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-20 23:34:54 +0900

    Updated

---
+++ xml-rest-api/restxml/src/main/java/com/github/hobbylabs/rest/restxml/RestxmlApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.rest.restxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestxmlApplication.class, args);
+	}
+
+}
---
+++ xml-rest-api/restxml/src/test/java/com/github/hobbylabs/rest/restxml/RestxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.rest.restxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
