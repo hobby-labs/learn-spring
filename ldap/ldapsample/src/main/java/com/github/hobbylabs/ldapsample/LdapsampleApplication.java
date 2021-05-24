9586890ac58fed2841c778c929278322f0673ec5
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-24 22:19:25 +0900

    Added new project

---
+++ ldap/ldapsample/src/main/java/com/github/hobbylabs/ldapsample/LdapsampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.ldapsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapsampleApplication.class, args);
+	}
+
+}
