ad96710fe6ed7578da7bfc39e4d10e44e7d586f4
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-26 02:05:17 +0900

    Added connection pooling

--- ldap/ldapsample/src/main/java/com/github/hobbylabs/ldapsample/configuration/LdapSampleConfiguration.java
+++ ldap/ldapsample/src/main/java/com/github/hobbylabs/ldapsample/configuration/LdapSampleConfiguration.java
@@ -2,8 +2,10 @@ package com.github.hobbylabs.ldapsample.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.pool.factory.PoolingContextSource;
 
 @Configuration
 public class LdapSampleConfiguration {
@@ -13,6 +15,7 @@ public class LdapSampleConfiguration {
         contextSource.setUrl("ldap://localhost:389");
         contextSource.setUserDn("cn=Manager,dc=mysite,dc=example,dc=com");
         contextSource.setPassword("secret");
+        contextSource.setPooled(true);  // This enables single connection pooling
 
         return contextSource;
     }
