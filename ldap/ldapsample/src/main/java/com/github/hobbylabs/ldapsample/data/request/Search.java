98ab57d652c2b21910bd2e5542709f3cf0f35b08
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-26 09:07:45 +0900

    Omit unused import statement

--- ldap/ldapsample/src/main/java/com/github/hobbylabs/ldapsample/data/request/Search.java
+++ ldap/ldapsample/src/main/java/com/github/hobbylabs/ldapsample/data/request/Search.java
@@ -1,13 +1,7 @@
 package com.github.hobbylabs.ldapsample.data.request;
 
 import lombok.Data;
-import org.springframework.ldap.filter.AbstractFilter;
-import org.springframework.ldap.filter.AndFilter;
-import org.springframework.ldap.filter.EqualsFilter;
-
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import java.lang.reflect.Field;
 
 @Data
 public class Search {
