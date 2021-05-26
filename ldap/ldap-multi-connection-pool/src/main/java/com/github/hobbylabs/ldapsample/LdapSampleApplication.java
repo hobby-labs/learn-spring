02d26c4b0afcef317d6eece560303de3022c6cf5
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-27 05:43:03 +0900

    Updated

--- ldap/ldap-multi-connection-pool/src/main/java/com/github/hobbylabs/ldapsample/LdapSampleApplication.java
+++ ldap/ldap-multi-connection-pool/src/main/java/com/github/hobbylabs/ldapsample/LdapSampleApplication.java
@@ -2,8 +2,11 @@ package com.github.hobbylabs.ldapsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class LdapSampleApplication {
 
 	public static void main(String[] args) {
