9492785eb98114bc74977e1cb758843e609f1fea
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-29 19:22:15 +0900

    Modified to apply pool configs

--- ldap/ldap-with-configuration-class/src/main/java/com/github/hobbylabs/ldapsample/configuration/LdapSampleConfiguration.java
+++ ldap/ldap-with-configuration-class/src/main/java/com/github/hobbylabs/ldapsample/configuration/LdapSampleConfiguration.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
-import org.springframework.ldap.pool.factory.PoolingContextSource;
+import org.springframework.ldap.pool2.factory.PoolConfig;
+import org.springframework.ldap.pool2.factory.PooledContextSource;
+import org.springframework.ldap.pool2.validation.DefaultDirContextValidator;
+import org.springframework.ldap.transaction.compensating.manager.TransactionAwareContextSourceProxy;
 
 @Configuration
 public class LdapSampleConfiguration {
@@ -20,18 +23,59 @@ public class LdapSampleConfiguration {
     @Value("${ldap.password}")
     private String ldapPassword;
 
-    @Value("${ldap.pooled}")
-    private Boolean ldapPooled;
+    @Value("${ldap.pool.enabled}")
+    private Boolean ldapPoolEnabled;
+
+    @Value("${ldap.pool.testOnBorrow}")
+    private Boolean ldapPoolTestOnBorrow;
+
+    @Value("${ldap.pool.maxTotalPerKey}")
+    private Integer maxTotalPerKey;
+
+    @Value("${ldap.pool.maxIdlePerKey}")
+    private Integer maxIdlePerKey;
+
+    @Value("${ldap.pool.timeBetweenEvictionRunsMillis}")
+    private Long timeBetweenEvictionRunsMillis;
+
+    @Value("${ldap.pool.minIdlePerKey}")
+    private Integer minIdlePerKey;
 
     @Bean
-    public LdapContextSource contextSource() {
-        LdapContextSource contextSource = new LdapContextSource();
-        contextSource.setUrl(ldapUrl);
-        contextSource.setUserDn(bindDn);
-        contextSource.setPassword(ldapPassword);
-        contextSource.setPooled(ldapPooled);  // This enables single connection pooling
+    public ContextSource contextSource() {
+        LdapContextSource ldapContextSource = new LdapContextSource();
+        ldapContextSource.setUrl(ldapUrl);
+        ldapContextSource.setUserDn(bindDn);
+        ldapContextSource.setPassword(ldapPassword);
+        ldapContextSource.afterPropertiesSet();
 
-        return contextSource;
+        if (ldapPoolEnabled != null && ldapPoolEnabled == true) {
+            return createPoolContextSource(ldapContextSource);
+        }
+
+        return ldapContextSource;
+    }
+
+    /**
+     * Create ContextSource that is enabled pool2 config.
+     * @param ldapContextSource LdapContextSource
+     * @return ContextSource that has pooling configuration.
+     */
+    public ContextSource createPoolContextSource(LdapContextSource ldapContextSource) {
+        PoolConfig poolConfig = new PoolConfig();
+        poolConfig.setTestOnBorrow(ldapPoolTestOnBorrow);
+        poolConfig.setMaxTotalPerKey(maxTotalPerKey);
+        poolConfig.setMaxIdlePerKey(maxIdlePerKey);
+        poolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        poolConfig.setMinIdlePerKey(minIdlePerKey);
+
+        PooledContextSource pcs = new PooledContextSource(poolConfig);
+        pcs.setContextSource(ldapContextSource);
+        pcs.setDirContextValidator(new DefaultDirContextValidator());
+
+        TransactionAwareContextSourceProxy contextSourceProxy = new TransactionAwareContextSourceProxy(pcs);
+
+        return contextSourceProxy;
     }
 
     @Bean
