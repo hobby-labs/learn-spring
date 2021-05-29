508875479554f9899753d8d71ea7905c5cc1eaa4
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-30 00:50:02 +0900

    Added new test cases

--- experiment/comparison-bean-and-builder-pattern/src/test/java/com/github/hobbylabs/experiment/comparisonbeanandbuilderpattern/DeserializabilityTest.java
+++ experiment/comparison-bean-and-builder-pattern/src/test/java/com/github/hobbylabs/experiment/comparisonbeanandbuilderpattern/DeserializabilityTest.java
@@ -1,28 +1,43 @@
 package com.github.hobbylabs.experiment.comparisonbeanandbuilderpattern;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.hobbylabs.experiment.comparisonbeanandbuilderpattern.data.LargeUserBean;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DeserializabilityTest {
     private static final String sampleData = "{"
-                + "{\"firstName\": \"Taro\"},"
-                + "{\"lastName\": \"Suzuki\"},"
-                + "{\"fullName\": \"Taro Suzuki\"},"
-                + "{\"age\": \"20\"},"
-                + "{\"country\": \"JP\"},"
-                + "{\"address\": \"Foo Bar Street\"},"
-                + "{\"mailAddress\": \"taro-suzuki@example.com\"},"
-                + "{\"phoneNumber\": \"000-000-0000\"},"
-                + "{\"height\": 170},"
-                + "{\"weight\": 65}"
+                + "\"firstName\": \"Taro\","
+                + "\"lastName\": \"Suzuki\","
+                + "\"fullName\": \"Taro Suzuki\","
+                + "\"age\": \"20\","
+                + "\"country\": \"JP\","
+                + "\"address\": \"Foo Bar Street\","
+                + "\"mailAddress\": \"taro-suzuki@example.com\","
+                + "\"phoneNumber\": \"000-000-0000\","
+                + "\"height\": 170,"
+                + "\"weight\": 65"
             + "}";
 
     private ObjectMapper mapper = new ObjectMapper();
 
     @Test
     @DisplayName("Bean should deserialize JSON string")
-    public void test0001() {
-        
+    public void test0001() throws JsonProcessingException {
+        LargeUserBean bean = mapper.readValue(sampleData, LargeUserBean.class);
+
+        assertEquals("Taro", bean.getFirstName());
+        assertEquals("Suzuki", bean.getLastName());
+        assertEquals("Taro Suzuki", bean.getFullName());
+        assertEquals(20, bean.getAge());
+        assertEquals("JP", bean.getCountry());
+        assertEquals("Foo Bar Street", bean.getAddress());
+        assertEquals("taro-suzuki@example.com", bean.getMailAddress());
+        assertEquals("000-000-0000", bean.getPhoneNumber());
+        assertEquals(170, bean.getHeight());
+        assertEquals(65, bean.getWeight());
     }
 }
