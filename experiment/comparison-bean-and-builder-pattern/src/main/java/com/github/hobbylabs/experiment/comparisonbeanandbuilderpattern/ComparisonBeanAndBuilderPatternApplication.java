5443a85962c77eed0348328b7200dac898f599fe
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-05-29 20:20:27 +0900

    Added new project

---
+++ experiment/comparison-bean-and-builder-pattern/src/main/java/com/github/hobbylabs/experiment/comparisonbeanandbuilderpattern/ComparisonBeanAndBuilderPatternApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.experiment.comparisonbeanandbuilderpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparisonBeanAndBuilderPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparisonBeanAndBuilderPatternApplication.class, args);
+	}
+
+}
