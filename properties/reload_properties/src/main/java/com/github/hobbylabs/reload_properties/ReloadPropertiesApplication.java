126ae870ad0c750f3891f7126ba4744aa3d96c10
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-08-28 11:06:07 +0900

    Added new project

---
+++ properties/reload_properties/src/main/java/com/github/hobbylabs/reload_properties/ReloadPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.reload_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReloadPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReloadPropertiesApplication.class, args);
+	}
+
+}
---
+++ properties/reload_properties/src/test/java/com/github/hobbylabs/reload_properties/ReloadPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.reload_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReloadPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
