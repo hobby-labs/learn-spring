0a4fe4177b29fa1740627ab2f2dfcfbb8c5a6199
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-08-29 00:00:10 +0900

    Added a new project

---
+++ properties/reload_properties_by_actuator_and_cloud/src/main/java/com/github/hobbylabs/reload_properties_by_actuator_and_cloud/ReloadPropertiesByActuatorAndCloudApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.reload_properties_by_actuator_and_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReloadPropertiesByActuatorAndCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReloadPropertiesByActuatorAndCloudApplication.class, args);
+	}
+
+}
---
+++ properties/reload_properties_by_actuator_and_cloud/src/test/java/com/github/hobbylabs/reload_properties_by_actuator_and_cloud/ReloadPropertiesByActuatorAndCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.reload_properties_by_actuator_and_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReloadPropertiesByActuatorAndCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
