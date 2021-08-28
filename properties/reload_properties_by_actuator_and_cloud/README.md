7ce01e1c967b85c7939dec5a39679848cd34038b
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-08-29 02:19:58 +0900

    Updated

--- properties/reload_properties_by_actuator_and_cloud/README.md
+++ properties/reload_properties_by_actuator_and_cloud/README.md
@@ -1,7 +1,31 @@
 # Reload properties by actuator and cloud
 
+## To run the application from the CLI
+
+```
+$ java -jar ./target/reload_properties_by_actuator_and_cloud-0.0.1-SNAPSHOT.jar \
+    --spring.config.location=file://${PWD}/target/classes/application.properties
+```
+
+## To run the application from the IntelliJ
+You can run the application from the IntelliJ by right-clicking ReloadPropertiesApplication from the Project pain then click `Run` on the context menu.
+
+## Testing
+Run continuous request from the command line like below and you can see a current value of `test.message` on your log terminal.
 ```
 $ while sleep 1; do curl http://localhost:8080/api/v1/getReloadProperties; done
+```
+
+You can confirm reloading the properties by modifying a value of `test.message` in `application.properties` located in `target/classes` directory and request the endpoint `/actuator/refresh`.
+
+* ./target/classes/application.properties
+```
+# You can change any values as you like
+test.message=foo bar baz
+```
+
+```
+$ curl -X POST http://localhost:8080/actuator/refresh
 ```
 
 # Reference
--- properties/reload_properties_by_actuator_and_cloud/src/main/java/com/github/hobbylabs/reload_properties_by_actuator_and_cloud/services/ReloadPropertiesByActuatorAndCloudService.java
+++ properties/reload_properties_by_actuator_and_cloud/src/main/java/com/github/hobbylabs/reload_properties_by_actuator_and_cloud/services/ReloadPropertiesByActuatorAndCloudService.java
@@ -3,16 +3,22 @@ package com.github.hobbylabs.reload_properties_by_actuator_and_cloud.services;
 import com.github.hobbylabs.reload_properties_by_actuator_and_cloud.properties.reloading.beans.ConfigurationPropertiesRefreshConfigBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RefreshScope
 public class ReloadPropertiesByActuatorAndCloudService {
 
-    @Autowired
-    private ConfigurationPropertiesRefreshConfigBean configurationPropertiesRefreshConfigBean;
+    private String message;
+
+    public ReloadPropertiesByActuatorAndCloudService(@Value("${application.test.message}") String message) {
+        this.message = message;
+    }
 
     public void getReloadProperties() {
-        log.info("test.message=\"" + configurationPropertiesRefreshConfigBean.getMessage() + "\"");
+        log.info("application.test.message=\"" + message + "\"");
     }
 }
