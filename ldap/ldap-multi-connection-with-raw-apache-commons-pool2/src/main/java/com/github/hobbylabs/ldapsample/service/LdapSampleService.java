cd3196b4267abab5ef2d0d502b564c3e19069f2e
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2023-07-29 18:32:43 +0900

    Added a new function

--- ldap/ldap-multi-connection-with-raw-apache-commons-pool2/src/main/java/com/github/hobbylabs/ldapsample/service/LdapSampleService.java
+++ ldap/ldap-multi-connection-with-raw-apache-commons-pool2/src/main/java/com/github/hobbylabs/ldapsample/service/LdapSampleService.java
@@ -1,12 +1,14 @@
 package com.github.hobbylabs.ldapsample.service;
 
+import com.github.hobbylabs.ldapsample.data.People;
+import com.github.hobbylabs.ldapsample.data.request.RequestQueryRoot;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LdapSampleService {
 
-    public LdapSampleService() {
-
+    public People getLdapDataByQuery(RequestQueryRoot requestedQueryRoot) {
+        // TODO:
+        return null;
     }
-
 }
